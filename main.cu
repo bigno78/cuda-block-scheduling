9463445fcdaa366274bb5ba646015b94a51203cc
Miroslav Demek <xdemek@fi.muni.cz>
2023-05-23 16:34:21 +0200

    Implement a basic ELL kernel

--- main.cu
+++ main.cu
@@ -68,12 +68,89 @@ std::vector<T> transfer_to_host(const T* ptr, int count)
 }
 
 
-__global__ void kernel() {}
+__global__ void kernel(int rows, int cols, int max_nnz_per_row,
+                       const float* values, const int* column_indices,
+                       const float*x, float*y)
+{
+    int row = blockIdx.x*blockDim.x + threadIdx.x;
+
+    if (row < rows)
+    {
+        float sum = 0.0f;
+        for (int i = 0; i < max_nnz_per_row; ++i)
+        {
+            int col = column_indices[i*rows + row];
+            if (0 <= col && col < cols)
+                sum += values[i*rows + row]*x[col];
+        }
+        y[row] = sum;
+    }
+}
+
+struct ELLMatrix
+{
+    int rows;
+    int cols;
+    int max_nnz_per_row;
+
+    std::vector<float> values;
+    std::vector<int> indices;
+};
+
+ELLMatrix initialize_matrix(int rows, int cols, int max_nnz_per_row)
+{
+    ELLMatrix mat { rows, cols, max_nnz_per_row };
+
+    mat.values.resize(rows*max_nnz_per_row, 1);
+    mat.indices.resize(rows*max_nnz_per_row);
+
+    int col = 0;
+    for (int i = 0; i < rows; ++i)
+    {
+        for (int j = 0; j < max_nnz_per_row; ++j)
+        {
+            mat.values[j*rows + i] = (i % 3) + 1;
+            mat.indices[j*rows + i] = col;
+            col = (col + 1) % cols;
+        }
+    }
+
+    return mat;
+}
+
+std::vector<float> initialize_x(int cols)
+{
+    return std::vector<float>(cols, 1);
+}
+
+void call_kernel()
+{
+    int n = 1024*1024;
+    int max_nnz_per_row = 32;
+
+    ELLMatrix mat = initialize_matrix(n, n, max_nnz_per_row);
+    auto x = initialize_x(n);
+
+    auto values_dptr = transfer_to_device(mat.values);
+    auto indices_dptr = transfer_to_device(mat.indices);
+    auto x_dptr = transfer_to_device(x);
+    auto y_dptr = device_malloc<float>(n);
+
+    int block_size = 256;
+    int grid_size = (n + block_size - 1)/block_size;
+
+    kernel<<<grid_size, block_size>>>(n, n, max_nnz_per_row,
+                                      values_dptr.get(), indices_dptr.get(),
+                                      x_dptr.get(), y_dptr.get());
+
+    std::vector<float> y = transfer_to_host(y_dptr.get(), n);
+
+    for (int i = 0 ; i < 20; ++i)
+        std::cout << y[i] << "\n";
+}
+
 
 int main()
 {
-    std::vector<int> v1 = { 1, 2, 3, 4 };
-    auto p = transfer_to_device(v1);
-    auto v2 = transfer_to_host(p.get(), v1.size());
-    std::cout << v2[0] << " " << v2[1] << " " << v2[2] << " " << v2[3];
+    call_kernel();
 }
